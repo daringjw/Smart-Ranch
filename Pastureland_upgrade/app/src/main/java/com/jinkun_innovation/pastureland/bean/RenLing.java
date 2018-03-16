e381eaa93a4f400713d353341e97455dffec4874
guan <909569736@qq.com>
2018-03-16 17:18:54 +0800

    renling fake data

---
+++ Pastureland_upgrade/app/src/main/java/com/jinkun_innovation/pastureland/bean/RenLing.java
@@ -0,0 +1,10 @@
+package com.jinkun_innovation.pastureland.bean;
+
+/**
+ * Created by Guan on 2018/3/16.
+ */
+
+public class RenLing {
+
+
+}
--- Pastureland_upgrade/app/src/main/java/com/jinkun_innovation/pastureland/ui/fragment/RenlingFragment.java
+++ Pastureland_upgrade/app/src/main/java/com/jinkun_innovation/pastureland/ui/fragment/RenlingFragment.java
@@ -1,19 +1,47 @@
 package com.jinkun_innovation.pastureland.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.zxing.client.android.CaptureActivity2;
 import com.jinkun_innovation.pastureland.R;
+import com.scwang.smartrefresh.header.FunGameHitBlockHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.constant.SpinnerStyle;
+import com.scwang.smartrefresh.layout.footer.BallPulseFooter;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 /**
  * Created by Guan on 2018/3/15.
  */
 
-public class RenlingFragment extends Fragment{
+public class RenlingFragment extends Fragment {
+
+    private static final int SCAN_REQUEST_CODE = 100;
+
+
+    private void startScanActivity() {
+        Intent intent = new Intent(getActivity(), CaptureActivity2.class);
+        intent.putExtra(CaptureActivity2.USE_DEFUALT_ISBN_ACTIVITY, true);
+        intent.putExtra("inputUnable", 0);
+        intent.putExtra("lightUnable", 1);
+        intent.putExtra("albumUnable", 1);
+        intent.putExtra("cancleUnable", 1);
+        startActivityForResult(intent, SCAN_REQUEST_CODE);
+
+    }
+
 
 
     @Nullable
@@ -23,9 +51,114 @@ public class RenlingFragment extends Fragment{
                              @Nullable Bundle savedInstanceState) {
 
 
-        View view= View.inflate(getActivity(), R.layout.fragment_claim,null);
+        View view = View.inflate(getActivity(), R.layout.fragment_claim, null);
+
+        ImageView ivAdd = view.findViewById(R.id.ivAdd);
+        ivAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                startScanActivity();
+
+            }
+        });
+
+
+        RefreshLayout refreshLayout = view.findViewById(R.id.refreshLayout);
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                refreshlayout.finishRefresh(2000/*,false*/);//传入false表示刷新失败
+            }
+        });
+
+        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+
+
+                refreshLayout.finishLoadMore(2000/*,false*/);//传入false表示加载失败
+
+            }
+        });
+
+        FunGameHitBlockHeader funGameHitBlockHeader = new FunGameHitBlockHeader(getActivity());
+        //设置 Header 为 Material样式
+        refreshLayout.setRefreshHeader(funGameHitBlockHeader);
+        //设置 Footer 为 球脉冲
+        refreshLayout.setRefreshFooter(new BallPulseFooter(getActivity())
+                .setSpinnerStyle(SpinnerStyle.Scale));
+
+
+        mRecyclerView = view.findViewById(R.id.my_recycler_view);
+//创建默认的线性LayoutManager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+//如果可以确定每个item的高度是固定的，设置这个选项可以提高性能
+        mRecyclerView.setHasFixedSize(true);
+//创建并设置Adapter
+        mAdapter = new MyAdapter(getDummyDatas());
+        mRecyclerView.setAdapter(mAdapter);
+
 
         return view;
+
+    }
+
+    private String[] getDummyDatas() {
+
+        String[] arr = {"北京", "上海", "广州", "深圳"};
+
+        return arr;
+
+    }
+
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private MyAdapter mAdapter;
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        public String[] datas = null;
+
+        public MyAdapter(String[] datas) {
+            this.datas = datas;
+        }
+
+        //创建新View，被LayoutManager所调用
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_claim, viewGroup, false);
+            ViewHolder vh = new ViewHolder(view);
+            return vh;
+        }
+
+
+        //将数据与界面进行绑定的操作
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, int position) {
+
+//            viewHolder.mTextView.setText(datas[position]);
+
+        }
+
+        //获取数据的数量
+        @Override
+        public int getItemCount() {
+            return 100;
+        }
+
+
+        //自定义的ViewHolder，持有每个Item的的所有界面元素
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView mTextView;
+
+            public ViewHolder(View view) {
+                super(view);
+//                mTextView = view.findViewById(R.id.tvClaim);
+
+            }
+
+        }
 
     }
 
